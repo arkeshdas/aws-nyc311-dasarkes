1264208d43954ac640448fd45529aaaf03a74cb2
arkeshdas <dasarkes@msu.edu>
2026-04-28 14:14:18 -0400

    added blurb to the stakeholder query file

--- sql/athena_to_modeling.sql
+++ sql/athena_to_modeling.sql
@@ -1,39 +1,83 @@
+/*
+This query creates the modeling table for predicting complaint resolution time.
+
+Business question:
+How long will a complaint take to be resolved, and what factors influence that?
+
+Target variable:
+days_to_close (difference between created_date and closed_date)
+
+Features:
+agency, borough, problem, incident_zip, day_of_week, hour_of_day, same_day_complaint_volume
+
+Transformations:
+- Parse created_date and closed_date into timestamps
+- Extract day_of_week and hour_of_day
+- Compute same_day_complaint_volume using a window function
+- Compute days_to_close using date_diff
+
+Filtering:
+- Remove rows with blank closed_date
+- Keep only valid NYC boroughs
+- Restrict resolution time to 0–365 days
+
+Modeling approach:
+Regression, since the target variable is continuous
+*/
+
 CREATE TABLE nyc311_db.resolution_time_modeling AS
 SELECT
     agency,
     borough,
     problem,
     incident_zip,
-    day_of_week(date_parse(created_date, '%Y-%m-%d %H:%i:%s'))  AS day_of_week,
-    hour(date_parse(created_date, '%Y-%m-%d %H:%i:%s'))          AS hour_of_day,
-    -- Count of same complaint type filed on same day (proxy for surge volume)
+
+    day_of_week(date_parse(created_date, '%Y-%m-%d %H:%i:%s')) AS day_of_week,
+
+    hour(date_parse(created_date, '%Y-%m-%d %H:%i:%s')) AS hour_of_day,
+
     COUNT(*) OVER (
         PARTITION BY agency, problem,
                      DATE(date_parse(created_date, '%Y-%m-%d %H:%i:%s'))
     ) AS same_day_complaint_volume,
-    -- Continuous target: days to close
-    date_diff('day',
+
+    date_diff(
+        'day',
         date_parse(created_date, '%Y-%m-%d %H:%i:%s'),
-        date_parse(closed_date,  '%Y-%m-%d %H:%i:%s')) AS days_to_close
+        date_parse(closed_date,  '%Y-%m-%d %H:%i:%s')
+    ) AS days_to_close
+
 FROM nyc311_db.complaints
 WHERE closed_date <> ''
-  AND borough IN ('BROOKLYN','QUEENS','BRONX','MANHATTAN','STATEN ISLAND')
-  AND date_diff('day',
+  AND borough IN ('BROOKLYN', 'QUEENS', 'BRONX', 'MANHATTAN', 'STATEN ISLAND')
+  AND date_diff(
+        'day',
         date_parse(created_date, '%Y-%m-%d %H:%i:%s'),
-        date_parse(closed_date,  '%Y-%m-%d %H:%i:%s')) BETWEEN 0 AND 365;
-
----- I am answering the stakeholder questions: Agency directors want a tool that estimates expected resolution time at the moment a complaint is filed, so they can set realistic expectations with residents. What factors drive that time?
-
-
--- What would the **target variable** be? (i.e., what would you be trying to predict)?
--- the target variable is the resolution time, so that is what I am trying to predict
-
--- What **features** should be included in the model?
--- I think that since this is an exploratory analysis, for now all of the features in the data should be included, because I don't know what is and isn't significant just yet.
-
--- What **aggregations** or **transformations** are needed to create the features and target variable?
--- This isn't really a transformation but I should keep in mind that variables like zip, burough and agency and problem are not continous veriables, and I might need to convert them into columns like BRONX_True, etc as binary 1 0 variables. 
+        date_parse(closed_date,  '%Y-%m-%d %H:%i:%s')
+      ) BETWEEN 0 AND 365;
 
 
--- What sort of **modeling approach** would be appropriate for the question and data you've created? (e.g., classification, regression, clustering)
--- I think that the cleanest and most interpretable thing would be to just do a MLS regression model. I could potentially do Backwards Selection and start with the full model, maybe even compare it to forward selection and stuff. Then, I will use coorelation statistics to determine the best model (in terms of over all predictions), the most efficient (compromise) model, adn alos just answer which features are the most significant.
+/*
+Validation query:
+Preview the modeling table to confirm columns and transformations look correct.
+*/
+
+SELECT *
+FROM nyc311_db.resolution_time_modeling
+LIMIT 10;
+
+/*
+expected result:
+#
+agency  borough        problem              incident_zip  day_of_week  hour_of_day  same_day_complaint_volume  days_to_close
+1 DCWP   BRONX          Consumer Complaint   10455         5            13           19                         33
+2 DCWP   BROOKLYN       Consumer Complaint   11207         5            11           19                         4
+3 DCWP   BROOKLYN       Consumer Complaint   11234         5            3            19                         34
+4 DCWP   BRONX          Consumer Complaint   10454         5            11           19                         33
+5 DCWP   QUEENS         Consumer Complaint   11423         5            15           19                         33
+6 DCWP   BRONX          Consumer Complaint   10470         5            16           19                         33
+7 DCWP   BRONX          Consumer Complaint   10467         5            14           19                         3
+8 DCWP   BROOKLYN       Consumer Complaint   11214         5            13           19                         33
+9 DCWP   QUEENS         Consumer Complaint   11354         5            9            19                         4
+10 DCWP  BROOKLYN       Consumer Complaint   11214         5            16           19                         33
+*/
